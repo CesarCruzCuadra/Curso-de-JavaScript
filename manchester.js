5a87bc96f9e1abf50f7c7698352f45845209d10c
CesarCruzCuadra <cesar.cruz-ext@dominion-global.com>
2022-02-23 18:32:09 -0600

    Se agrego mas condicionales.

--- manchester.js
+++ manchester.js
@@ -1,23 +1,69 @@
 
 dineroAlejandro = prompt("Cuanto dinero tienes Alejandro?");
+dineroMario = prompt("Cuanto dinero tienes Mario?");
+dineroPedro = prompt("Cuanto dinero tienes Pedro?");
  
 if (dineroAlejandro >= 0.6 && dineroAlejandro < 1){
-    alert("Comprate el helado de mani")
+    alert("Comprate el helado de mani Alejandro")
 }
 else if(dineroAlejandro >= 1 && dineroAlejandro < 1.6){
-    alert("Comprate el helado de vainia ")
+    alert("Comprate el helado de vainia Alejandro ")
 }
 else if(dineroAlejandro >= 1.6 && dineroAlejandro < 1.7){
-    alert("Comprate el helado de chocolate ")
+    alert("Comprate el helado de chocolate Alejandro ")
 }
 else if(dineroAlejandro >= 1.7 && dineroAlejandro < 1.8){
-    alert("Comprate el helado de menta ")
+    alert("Comprate el helado de menta Alejandro ")
 }
 else if(dineroAlejandro >= 1.8 && dineroAlejandro < 2.9){
-    alert("Comprate el helado de napolitano ")
+    alert("Comprate el helado de napolitano Alejandro  ")
 }
 else if (dineroAlejandro >= 2.9){
-alert("Comprate un chocochoco");
+alert("Comprate un chocochoco Alejandro ");
 } else{
-   alert("Lo sentimos no cuentas con suficiente dinero");
+   alert("Lo sentimos no cuentas con suficiente dinero Alejandro ");
+}
+
+if (dineroMario >= 0.6 && dineroMario < 1){
+    alert("Comprate el helado de mani Mario")
+}
+else if(dineroMario >= 1 && dineroMario < 1.6){
+    alert("Comprate el helado de vainia Mario")
+}
+else if(dineroMario >= 1.6 && dineroMario < 1.7){
+    alert("Comprate el helado de chocolate Mario")
+}
+else if(dineroMario >= 1.7 && dineroMario < 1.8){
+    alert("Comprate el helado de menta Mario ")
+}
+else if(dineroMario >= 1.8 && dineroMario < 2.9){
+    alert("Comprate el helado de napolitano Mario")
+}
+else if (dineroMario >= 2.9){
+alert("Comprate un chocochoco Mario");
+} else{
+   alert("Lo sentimos no cuentas con suficiente dinero Mario");
+}
+
+
+
+ if (dineroPedro >= 0.6 && dineroPedro < 1){
+    alert("Comprate el helado de mani Mario Pedro ")
+}
+else if(dineroPedro >= 1 && dineroPedro < 1.6){
+    alert("Comprate el helado de vainia Pedro ")
+}
+else if(dineroPedro >= 1.6 && dineroPedro < 1.7){
+    alert("Comprate el helado de chocolate Pedro ")
+}
+else if(dineroPedro >= 1.7 && dineroPedro < 1.8){
+    alert("Comprate el helado de menta Pedro ")
+}
+else if(dineroPedro >= 1.8 && dineroPedro < 2.9){
+    alert("Comprate el helado de napolitano Pedro")
+}
+else if (dineroPedro >= 2.9){
+alert("Comprate un chocochoco Pedro ");
+} else{
+   alert("Lo sentimos no cuentas con suficiente dinero Pedro");
 }
